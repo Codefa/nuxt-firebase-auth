5420f12a8f06ad599947e18a81d116046cb7127b
SHARON <sharushj690@gmail.com>
2018-09-26 10:57:27 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'apiKey': 'AIzaSyDRJRTxSGuzCWPAmK1SWJkvNcuSttJDn6g',
-  'authDomain': 'dev-meetup-2b62b.firebaseapp.com',
-  'databaseURL': 'https://dev-meetup-2b62b.firebaseio.com',
-  'projectId': 'dev-meetup-2b62b',
-  'storageBucket': 'dev-meetup-2b62b.appspot.com',
-  'messagingSenderId': '681259683654'
-}
